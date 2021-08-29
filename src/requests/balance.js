47e063896879a738fa415e962b09f31b4c72969a
Lais Frigério <laiss@gazetadopovo.com.br>
2021-08-28 22:00:33 -0300

    Refactor requests to use repositories

--- src/requests/balance.js
+++ src/requests/balance.js
@@ -1,22 +1,18 @@
 const express = require('express')
-const db = require('../database')
+const getBalance = require('../repositories/get-balance')
 
 const router = express.Router()
 
 router.get('/balance', async (req, res) => {
   const { account_id } = req.query
 
-  try {
-    const account = db.get('accounts').find({ id: account_id }).value()
+  const account = getBalance(account_id)
 
-    if (!account) {
-      return res.status(404).json(0)
-    }
-
-    return res.status(200).json(account.balance)
-  } catch (err) {
-    return res.status(500).json({ success: false, message: err.message })
+  if (!account) {
+    return res.status(404).json(0)
   }
+
+  return res.status(200).json(account.balance)
 })
 
 module.exports = router
